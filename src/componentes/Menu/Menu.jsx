f970b1917935e8b542e7f9dcf9477eb58517dcb4
Nicole Cassiano <nicolecassdevf@gmail.com>
2026-05-07 23:43:57 -0300

    Adiciona seção menu de navegação

---
+++ src/componentes/Menu/Menu.jsx
@@ -0,0 +1,22 @@
+import "./Menu.css";
+
+function Menu() {
+  return (
+    <header className="Menu">
+      <div className="container_menu">
+        <div className="container_nome">
+          <h4>Nicole Cass</h4>
+        </div>
+        <div className="container_nav">
+          <nav>
+            <a href="">Habilidades</a>
+            <a href="">Meus projetos</a>
+            <a href="">Contato</a>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Menu;
--- src/componentes/Navbar/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import "./Navbar.css"
-
-function Navbar() {
-    return (
-        <div>oiiiiiiiiiiii</div>
-    )
-}
-
-export default Navbar
